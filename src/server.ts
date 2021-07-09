4d3b2068ea80bba40eada9e4fbccfe83048186ce
Sayer <sayer@worldtree.io>
2021-07-09 15:30:54 -0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,12 +13,17 @@ export const createServer = (
 ): express.Express => {
   const app = express();
 
-  app.use((req, res, next) => {
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+  app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
     );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+
     next();
   });
   app.use(express.json());
